8c6debd523310a00b8fc6408afb9d2ba13a2a694
Baraldi97 <baraldi106@gmail.com>
2025-07-22 17:16:11 -0300

    att libft.h

---
+++ core/libft.h
@@ -0,0 +1,8 @@
+int ft_isalpha(char c);
+int ft_isdigit(int nb);
+int ft_isalnum(char c);
+int ft_isascii(int c);
+int ft_isprint(char c);
+int ft_strlen(char *str);
+void    *ft_memset(void *s, int c, size_t n);
+void    ft_bzero(void *s, size_t n);
