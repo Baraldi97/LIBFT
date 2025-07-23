5ca23af656f2789d1f9d0dcf60e41c293336aab5
Baraldi97 <baraldi106@gmail.com>
2025-07-23 11:42:36 -0300

    att libft.h

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -10,7 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
+	unsigned char		*d;
+	const unsigned char *s;
+	size_t				i;
 	
+	 if (!dest && !src)
+        return (NULL);
+	if (n == 0)
+		return (dest);
+	d = (unsigned char *)dest;
+    s = (const unsigned char *)src;
+    i = 0;
+	
+	while(i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
 }
+// int	main(void)
+// {
+// 	char	name[8] = "Rodrigo";
+// 	char	new[8] = "";
+
+// 	printf("This is the var name: %s and this is the var new: %s\n", name, new);
+// 	ft_memcpy(new, name, 8);
+// 	printf("This is the var name: %s and this is the var new: %s\n", name, new);
+// 	return (0);
+// }
