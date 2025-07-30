608ffbb7ec822bd29315fbce03ba9f5e516fa63c
Rodrigo Baraldi Costa <baraldi106@gmail.com>
2025-07-30 16:25:27 -0300

    add revisei a função strlcat

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcosta <rcosta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/30 15:21:10 by rcosta            #+#    #+#             */
+/*   Updated: 2025/07/30 15:53:48 by rcosta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+size_t	ft_strlen(const char *str);
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
+	while (src[i] != '\0' && dst_len + i + 1 < dstsize)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
+}
+int	main(void)
+{
+	char	name[9] = " Baraldi";
+	char	surname[8] = "Rodrigo";
+	ft_strlcat(surname, name, 17);
+	printf("Concatenated name: %s\n", surname);
+	return(0);
+}
