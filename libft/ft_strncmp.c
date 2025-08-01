3db324364a582e0ced1fa45fadd71bf058347625
Baraldi97 <baraldi106@gmail.com>
2025-07-31 22:33:04 -0300

    add função strncmp

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 22:04:20 by marvin            #+#    #+#             */
+/*   Updated: 2025/07/31 22:04:20 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while (i < n && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+    {
+        i++;
+    }
+    if (i == n)
+        return(0);
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+// int main(void)
+// {
+//     char    name[8] = "Rodrigo";
+//     char    name_2[8] = "Rodrigo";
+//     int     result;
+
+//     result = ft_strncmp(name, name_2, 8);
+//     printf ("%d", result);
+//     return (0);
+// }
