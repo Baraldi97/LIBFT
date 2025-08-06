3df68cde60348378ef2aef854279031479a01080
Rodrigo Baraldi Costa <baraldi106@gmail.com>
2025-08-06 19:13:09 -0300

    calloc

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,24 @@
 /*   By: rcosta <rcosta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 17:03:29 by rcosta            #+#    #+#             */
-/*   Updated: 2025/08/06 17:03:30 by rcosta           ###   ########.fr       */
+/*   Updated: 2025/08/06 19:12:22 by rcosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	n_size;
+	void	*ptr;
+
+	n_size = nmemb * size;
+	if (nmemb != 0 && n_size / nmemb != size)
+		return (NULL);
+	ptr = ft_malloc(n_size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, n_size);
+	return (ptr);
+}
 
