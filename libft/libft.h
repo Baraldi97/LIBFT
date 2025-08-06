5bfae6f47ae860200a34d8f6b980718c187c6311
Rodrigo Baraldi Costa <baraldi106@gmail.com>
2025-08-06 14:58:51 -0300

    ajustando um pouco a libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -10,10 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stddef.h>
-#include <unistd.h>
-#include <ctype.h>
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <unistd.h>
+// #include <stdio.h>
+// #include <stddef.h>
+// #include <unistd.h>
+// #include <ctype.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 int		ft_isalpha(char c);
 int		ft_isdigit(int nb);
@@ -24,3 +34,5 @@ size_t	ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+#endif
