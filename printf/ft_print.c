9f98fc0d4491abed333e2494943d9772352e20a4
Baraldi97 <baraldi106@gmail.com>
2025-09-08 23:05:09 -0300

    att em casa

--- printf/ft_print.c
+++ printf/ft_print.c
@@ -31,30 +31,29 @@ int	print_str(char *s)
 		count ++; //Aqui é para ir incrementando o numero de bytes
 		s ++; //Aqui é para percorrer a string e poder ir incrementando mais
 	}
-	return *s;
+	return count;
 }
 
-int	print_digit(long	n, int	base)
+int	print_digit(long	n, int	base, char *base_chars)
 {
 	int		count;
-	char	*hexad;
 
-	hexad =  "0123456789abcdef";
 	if (n < 0) //Tratando o caso de numeros negativos.
 	{
 		write(1, "-", 1);
-		//Aqui, estou usando recursão, e o -n está assim, porque o sinal de menos foi escrito antes e preciso trabalhar com o numero positivo. 
-		return print_digit(-n, base) + 1;//Esse +1 no final é porquê estou colcando o sinal de menos na função. A print_digit me retorna o Nº dos digitos + esse sinal. 
+		count = 1;
+		n = -n;
 	}
 	else if (n < base) //Aqui, caso o numero seja menor que a base, eu só imprimo ele, utilizando meu array hexad, porque não preciso fazer conversão.
 	{
-		return print_char(hexad[n]);
+		return print_char(base_chars[n]);
 	}
 	else//Aqui fazemos a conversão dos numeros não negativos e maiores que a base, de acordo com a sua base. Para isso utilizamos recursão (preciso entender como essa recursão acontece aqui dentro)
 	{
-		count =  print_digit(n / base, base);
-		return count +  print_digit(n % base, base);
+		count =  print_digit(n / base, base, base_chars);
+		count =  print_digit(n % base, base, base_chars);
 	}
+	return count;
 }
 
 // Com essa função, vou fazer a identificação da letra c, d, s, x, X... e aplica a função para já converter o que precisa ser convertido. 
@@ -63,6 +62,8 @@ int	print_format(char type, va_list args) //Aqui eu preciso entender melhor o po
 	//Aqui a mesma coisa, entender o por quê usei o args dentro de va_arg.
 	//E entender um pouco melhor o Type Promotion, sei que por causa dele eu emprego int.// Mas não entendo 100% o por quê
 	int	count;
+	char *hex_lower = "0123456789abcdef";
+	char *hex_upper = "0123456789ABCDEF";
 	
 	count = 0;
 	if (type == 'c')
@@ -70,9 +71,17 @@ int	print_format(char type, va_list args) //Aqui eu preciso entender melhor o po
 	else if (type == 's')
 		count += print_str(va_arg(args, char *));
 	else if (type == 'd')
-		count += print_digit((long)va_arg(args, int), 10); //Usei o print digit para o meu int e hexadecimal, preciso entender melhor o porquê do cast explicito para long aqui e no hexadecimal também
+		count += print_digit((long)va_arg(args, int), 10, "0123456789"); //Usei o print digit para o meu int e hexadecimal, preciso entender melhor o porquê do cast explicito para long aqui e no hexadecimal também
+	else if (type == 'i')
+		count += print_digit((long)va_arg(args, int), 10, "0123456789");
+	else if (type == 'u')
+		count += print_digit((long)va_arg(args, int), 10, "0123456789");
 	else if (type == 'x')
-		count += print_digit((long)va_arg(args, unsigned int), 16); // o Unsigned aqui está sendo usado, porque no printf original, o numero hexadecimal não é aparece como esperamos para negativos! (PReciso entender um pouco melhor isso também)
+		count += print_digit((long)va_arg(args, unsigned int), 16, hex_lower); // o Unsigned aqui está sendo usado, porque no printf original, o numero hexadecimal não é aparece como esperamos para negativos! (PReciso entender um pouco melhor isso também)
+	else if (type == 'X')
+		count += print_digit((long)va_arg(args, unsigned int), 16, hex_upper);
+	else if (type == '%')
+		count += print_char('%');
 	else
 		write(1, "type", 1);
 	return (count);
@@ -100,6 +109,10 @@ int	ft_printf(const char *format, ...)
 
 int	main()
 {
-	ft_printf("My name is %s, and I'm %d years old, my age in Hexadeximal is %x\n", "Rodrigo", 28, 28);
+	char *word;
+
+	word = "Ro";
+	ft_printf("My name is %s, I'm %d years old, my age in Hexadeximal is %x, in %% CAPITAL is %X\n", word, 42, 42, 42);
+	printf("My name is %s, I'm %d years old, my age in Hexadeximal is %x, in %% CAPITAL is %X\n", word, 42, 42, 42);
 	return (0);
 }
